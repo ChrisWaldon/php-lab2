a9c75b9b228b5c7b31e2822979b47b39b93e5d79
christopherwaldon <cw@kspcs.com>
2015-04-21 16:10:55 -0400

    fixed typo in access

--- access.php
+++ access.php
@@ -1,7 +1,5 @@
 <?php
-echo 'trying to connect';
-$db = msql_connect('localhost', 'waldonck', '900494932');
+$db = mysql_connect('localhost', 'waldonck', '900494932');
 if (!$db) {
 	die('could not connect: '. mysql_error());
 }
-echo 'connection ran';

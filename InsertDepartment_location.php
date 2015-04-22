99c9ea24779e58765cf4f6541983d9b5e3df0580
christopherwaldon <cw@kspcs.com>
2015-04-21 21:30:14 -0400

    Added Department_locations.

---
+++ InsertDepartment_location.php
@@ -0,0 +1,25 @@
+<?php
+	include('access.php');	
+	$result = @mysql_query('insert into department(dnumber, dlocation) values ('
+			.$_POST['dnumber'].', "'
+			.$_POST['dlocation'].'");'
+			);
+	if (!$result) {
+		echo '<p>There was a problem inserting. Please try again.</p>';
+	} else {
+        echo '<p>Inserted Successfully</p>';
+	}
+?>
+<html>
+  <body>
+    <form action='InsertDepartment_location.php' method='post'>
+   	  <label for="fname">Department Number:</label>
+      <input type='text' name='dnumber' />
+      <br>
+   	  <label for="fname">Department Location:</label>
+      <input type='text' name='dlocation' />
+      <br>
+      <input type='submit' />
+    </form>
+  </body>
+</html>
